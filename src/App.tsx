f84db115ec5e5b85598b1491cb3016ff9e8242b5
ANON <anon@example.com>
2025-07-10 16:18:50 -0400

    Instrumenting with counter

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,13 @@ import logo from './logo.svg';
 import Search from './components/Search';
 import Results from './components/Results';
 import './App.css';
-
 function App() {
-  const [source, setSource] = useState('StackOverflow')
-  const [query, setQuery] = useState('')
-
-  return (
-    <div className="App">
+  console.log(window.globalCount++);
+  const [source, setSource] = useState('StackOverflow');
+  const [query, setQuery] = useState('');
+  return <div className="App">
       <Search source={source} setSource={setSource} query={query} setQuery={setQuery} />
       <Results source={source} query={query} />
-    </div>
-  );
+    </div>;
 }
-
 export default App;
--- src/components/None.tsx
+++ src/components/None.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import Nothing from "../assets/nothing.jpeg"
-import "../styles/None.css"
-
-
+import Nothing from "../assets/nothing.jpeg";
+import "../styles/None.css";
 function None() {
-  return (
-    <div className='none'>
+  console.log(window.globalCount++);
+  return <div className='none'>
         <img className='nothing' src={Nothing} alt="nothing" />
         <h1>No results found :\</h1>
-    </div>
-  );
+    </div>;
 }
-
 export default None;
